ad32ad8b41fc2b9ed1d11ec9e1de6e3a8b9cde00
eueulkpo-cpu <eueulkpo@gmail.com>
2026-06-15 17:28:25 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let velocidade = 10;
-
-document.addEventListener("touchmove" , () => {
-    velocidade *= 0.95; // reduz a velocidade em 5% a cada movimento
-    console.log("velocidade:", velocidade);
-});
