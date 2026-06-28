d2bb2be94b68e2efa93006793ca83953352e9a2e
eueulkpo-cpu <eueulkpo@gmail.com>
2026-06-28 09:54:22 -0400

    Add files via upload

---
+++ Otimiza.js
@@ -0,0 +1,19 @@
+let utimotempo =0;
+
+function loop(tempo) {
+    const delta = tempo - utimotempo;
+    utimoTempo = tempo;
+
+    //Atualiza so o necesasio
+
+    requestAnimationFrame(loop);
+};
+
+requestAnimationFrame(loop);
+
+const botao = document.getElementById("Aimbot");
+const botao = document.getElementById("status");
+
+botao.addEventListener("click"), function () {
+    status.texcontent = "on";
+}
