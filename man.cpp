932eb59c9a9d69f4d86920da37a73eb23cf54590
eueulkpo-cpu <eueulkpo@gmail.com>
2026-08-31 18:14:00 -0500

    Delete man.cpp

--- man.cpp
+++
@@ -1,9 +0,0 @@
-float limitar_sensibilidade(float valor_atual, float limitee_maximo) {
-    if (valor_atual > limitee_maximo ) {
-        return limitee_maximo;
-    }
-
-   float sensibilidade = 00.05f;
-   float limete = 00.06f;
-   return valor_atual + sensibilidade * (limitee_maximo - valor_atual) * limete;
-}
