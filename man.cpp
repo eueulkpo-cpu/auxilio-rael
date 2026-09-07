f267736305131052b0fd20d59855b9d58bec209b
eueulkpo-cpu <eueulkpo@gmail.com>
2026-09-07 11:41:12 -0500

    Add files via upload

---
+++ man.cpp
@@ -0,0 +1,51 @@
+  //AIM
+   18:00
+using UnityEngine;
+
+public class AimController : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    [Header("Configuração")]
+    public float sensitivity = 0.18f;
+    public float smoothness = 20f;
+
+    private float yaw;
+    private float pitch;
+
+    private Vector2 currentAim;
+    private Vector2 targetAim;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                targetAim += touch.deltaPosition * sensitivity;
+            }
+        }
+
+        currentAim = Vector2.Lerp(
+            currentAim,
+            targetAim,
+            smoothness * Time.deltaTime
+        );
+
+        yaw += currentAim.x;
+        pitch -= currentAim.y;
+
+        pitch = Mathf.Clamp(pitch, -80f, 80f);
+
+        cameraTransform.localRotation =
+            Quaternion.Euler(pitch, 0f, 0f);
+
+        transform.rotation =
+            Quaternion.Euler(0f, yaw, 0f);
+
+        targetAim = Vector2.zero;
+    }
+}
+}
